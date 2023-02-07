7529725c3abadd0fc7ffbdcefd0dc0d7855824c8
njosiahk <s10244345@connect.np.edu.sg>
2023-02-07 18:02:15 +0800

    final stretch part 1 (hopefully)

--- login2.js
+++ login2.js
@@ -1,26 +1,3 @@
-// // var settings = {
-// //     "async": true,
-// //     "crossDomain": true,
-// //     "url": "https://team7assg2-2b5b.restdb.io/rest/login",
-// //     "method": "GET",
-// //     "headers": {
-// //       "content-type": "application/json",
-// //       "x-apikey": "63d62e7f3bc6b255ed0c43df",
-// //       "cache-control": "no-cache"
-// //     }
-// //   }
-  
-// //   $.ajax(settings).done(function (response) {
-// //     console.log(response);
-// //   });
-
-
-
-
-
-
-
-
 //   // INITIALISE CONSTANTS
 // const dbURL = "https://team7assg2-2b5b.restdb.io/rest/login";
 // const APIKEY = "63d62e7f3bc6b255ed0c43df";
@@ -71,7 +48,6 @@
 
 //     });
 // });
-
 
 // // INPUT VALIDATION
 // function validateInput(email, pwd, dobStr, errorMsg) {
@@ -148,8 +124,6 @@
 //     //     });
 //     // });
 
-
-
 // // AJAX TO ACCESS DATABASE
 // function ajaxFuncGET() {
 //     return $.ajax({
@@ -197,7 +171,7 @@
 //       "processData": false,
 //       "data": JSON.stringify(jsondata)
 //     }
-    
+
 //     $.ajax(settings).done(function (response) {
 //       console.log(response);
 //       // response.map(account => {
@@ -235,35 +209,89 @@
 // });
 // }
 
+// $(document).ready(function () {
+//   $("#register").submit(function (e) {
+//     e.preventDefault();
+//     let email = $("#registerEmail").val();
+//     let password = $("#registerPassword").val();
+//     let name = $("#registerName").val();
+//     setTimeout(createAccount(name, email, password), 5000);
+//   });
+//   function createAccount(Name, Email, Password) {
+//     var jsondata = { name: Name, email: Email, password: Password };
+//     // var jsondata = {"field1": "xyz","field2": "abc"};
+//     var settings = {
+//       async: true,
+//       crossDomain: true,
+//       url: "https://team7njks-7c44.restdb.io/rest/login",
+//       method: "POST",
+//       headers: {
+//         "content-type": "application/json",
+//         "x-apikey": "63e217ae3bc6b255ed0c475f",
+//         "cache-control": "no-cache",
+//       },
+//       processData: false,
+//       data: JSON.stringify(jsondata),
+//     };
 
-$("document").ready(function(){
-  $("#register").submit(function(e){
+//     $.ajax(settings).done(function (response) {
+//       console.log(response);
+//     });
+//   }
+// });
 
-    e.preventDefault()
+//[STEP 0]: Make sure our document is A-OK
+$(document).ready(function () {
+  //what kind of interface we want at the start
+  const APIKEY = "63e217ae3bc6b255ed0c475f";
+  $(".successfulmsg").hide();
+  //[STEP 1]: Create our submit form listener
+  $("#register").submit(function (e) {
+    //prevent default action of the button
+    e.preventDefault();
+
+    //[STEP 2]: let's retrieve form data
+    //for now we assume all information is valid
+    //you are to do your own data validation
+    let name = $("#registerName").val();
     let email = $("#registerEmail").val();
     let password = $("#registerPassword").val();
-    let name = $("#registerName").val();
-    createAccount(name, email, password);
-  })
-  function createAccount(Name, Email, Password){
-    var jsondata = {"name": Name, "email": Email ,"password": Password,};
-    // var jsondata = {"field1": "xyz","field2": "abc"};
+
+    //[STEP 3]: get form values when user clicks on send
+    //Adapted from restdb api
+    let jsondata = {
+      name: name,
+      email: email,
+      password: password,
+      // score: 0,
+    };
+    console.log(jsondata);
+    //[STEP 4]: Create our AJAX settings. Take note of API key
     var settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://team7assg2-2b5b.restdb.io/rest/login",
-      "method": "POST",
-      "headers": {
+      async: true,
+      crossDomain: true,
+      url: "https://team7njks-7c44.restdb.io/rest/login",
+      method: "POST",
+      headers: {
         "content-type": "application/json",
-        "x-apikey": "63d62e7f3bc6b255ed0c43df",
-        "cache-control": "no-cache"
+        "x-apikey": "63e217ae3bc6b255ed0c475f",
+        "cache-control": "no-cache",
       },
-      "processData": false,
-      "data": JSON.stringify(jsondata)
-    }
-    
+      processData: false,
+      data: JSON.stringify(jsondata),
+    };
+
     $.ajax(settings).done(function (response) {
       console.log(response);
     });
-    }
-})
+    // beforeSend: function () {
+    //   // if (username === ""  password1 === ""  password2 == "" || email === "") {
+    //   //   $('.errormsg').text("Fields cannot be left blank");
+    //   // }
+    //   //@TODO use loading bar instead
+    //   //disable our button or show loading bar
+    //   //clear our form using the form id and triggering it's reset feature
+    //   // $("#form").trigger("reset");
+    // },
+  });
+});

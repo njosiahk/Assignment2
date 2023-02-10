c9a4dc9ece0f26b00710c77647805078d5c238ad
njosiahk <s10244345@connect.np.edu.sg>
2023-02-10 17:20:39 +0800

    leaderboard slight update

--- Javascript/game.js
+++ Javascript/game.js
@@ -75,11 +75,32 @@ $(document).ready(function () {
   // const APIKEY = "63e217ae3bc6b255ed0c475f"; //(nic)
   const APIKEY = "63d62e7f3bc6b255ed0c43df"; //(team7asg2)
 
+  let id = sessionStorage.getItem("id");
+
+  var jsondata = { score: score };
   var settings = {
     async: true,
     crossDomain: true,
-    url: "https://team7assg2-2b5b.restdb.io/rest/login?q={}&sort=moves&dir=1",
-    // url: "https://team7njks-7c44.restdb.io/rest/login?q={}&sort=moves&dir=1",
+    url: `https://team7assg2-2b5b.restdb.io/rest/login/${id}`,
+    method: "PATCH",
+    headers: {
+      "content-type": "application/json",
+      "x-apikey": APIKEY,
+      "cache-control": "no-cache",
+    },
+    processData: false,
+    data: JSON.stringify(jsondata),
+  };
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+
+  var settings = {
+    async: true,
+    crossDomain: true,
+    url: "https://team7assg2-2b5b.restdb.io/rest/login?q={}&sort=score&dir=1",
+    // url: "https://team7njks-7c44.restdb.io/rest/login?q={}&sort=scire&dir=1",
     method: "GET",
     headers: {
       "content-type": "application/json",
@@ -95,7 +116,7 @@ $(document).ready(function () {
       content = `${content}<tr id='${response[i]._id}'><td>${i + 1}</td><td>${
         response[i].name
       }</td>
-          <td>${response[i].moves}</td>`;
+          <td>${response[i].score}</td>`;
     }
     $("#leaderboard-list tbody").html(content);
 
--- Javascript/login1.js
+++ Javascript/login1.js
@@ -2,99 +2,94 @@
 // const dbURL = "https://team7njks-7c44.restdb.io/rest/login";
 const dbURL = "https://team7assg2-2b5b.restdb.io/rest/login";
 // const APIKEY = "63e217ae3bc6b255ed0c475f";
-const APIKEY ="63d62e7f3bc6b255ed0c43df"
-const image = document.createElement("img").src = "../HomePage/user.png";
-
+const APIKEY = "63d62e7f3bc6b255ed0c43df";
+const image = (document.createElement("img").src = "../HomePage/user.png");
 
 // MAIN CODE
 $(document).ready(function () {
-    let submitButton = $("#loginSubmit");
-    let errorMsg = $("#errorMessage");
-    let spinner = $("#spinner");
+  let submitButton = $("#loginSubmit");
+  let errorMsg = $("#errorMessage");
+  let spinner = $("#spinner");
 
-    
-    // WHEN USER CLICKS LOG IN BUTTON
-    submitButton.on("click", function (e) {
-        e.preventDefault();
+  // WHEN USER CLICKS LOG IN BUTTON
+  submitButton.on("click", function (e) {
+    e.preventDefault();
 
-        let uEmail = $("#loginEmail").val();
-        console.log(uEmail);
-        let uPwd = $("#loginPassword").val();
-        
-        errorMsg.hide();
-        spinner.css('display', 'block');
+    let uEmail = $("#loginEmail").val();
+    console.log(uEmail);
+    let uPwd = $("#loginPassword").val();
 
-        ajaxFuncGET().done(function (response) {
-            spinner.css('display', 'none');
+    errorMsg.hide();
+    spinner.css("display", "block");
 
-            let accountExists = false;
-            response.map(account => {
-                // let responseID = account._id;
-                let responseEmail = account.email;
-                console.log(responseEmail);
-                let responsePwd = account.password
-                
-                
+    ajaxFuncGET().done(function (response) {
+      spinner.css("display", "none");
 
-                if (uEmail === responseEmail && uPwd === responsePwd) {
-                    let username = account.name
-                    localStorage.setItem("name", username)
-                    localStorage.setItem("userAccount", uEmail);
-                    localStorage.setItem("loginStatus", true);
-                    alert("Login successful!");
-                    // window.location.assign("blastem.html");
-                    let name = localStorage.getItem("name");
-                    console.log(name);
-                    accountExists = true;
-                    window.location = "index.html"
-                    // console.log(localstorage.getItem("name"));
+      let accountExists = false;
+      response.map((account) => {
+        // let responseID = account._id;
+        let responseEmail = account.email;
+        console.log(responseEmail);
+        let responsePwd = account.password;
+        let id = account._id;
 
-                    // if (document.title == "VALORANT WIKI: HOME"){
-                    //     let user = document.getElementById("paraname");
-                    //     console.log(user);
-                    //     user.innerHTML += localStorage.getItem("name");
-                    // }
+        if (uEmail === responseEmail && uPwd === responsePwd) {
+          let username = account.name;
+          localStorage.setItem("name", username);
+          localStorage.setItem("userAccount", uEmail);
+          localStorage.setItem("loginStatus", true);
+          alert("Login successful!");
+          // window.location.assign("blastem.html");
+          let name = localStorage.getItem("name");
+          console.log(name);
+          accountExists = true;
+          window.location = "index.html";
+          // console.log(localstorage.getItem("name"));
 
-                    var loginUsername = username
-                    window.loginUsername = username 
-                    sessionStorage.setItem("name", loginUsername);
-                    console.log(loginUsername);
+          // if (document.title == "VALORANT WIKI: HOME"){
+          //     let user = document.getElementById("paraname");
+          //     console.log(user);
+          //     user.innerHTML += localStorage.getItem("name");
+          // }
 
-                    // let user = document.getElementById("paraname");
-                    // console.log(user);
-                    // user.innerHTML += localStorage.getItem("name");
-                    // accountExists = true;
-                    // window.location.href = "index.html"
+          var loginUsername = username;
+          window.loginUsername = username;
+          sessionStorage.setItem("name", loginUsername);
+          sessionStorage.setItem("id", JSON.stringify(id));
+          console.log(loginUsername);
 
-                    
-                   
-                    // console.log(localStorage.getItem("userAccount"));
-                }
-            });
+          // let user = document.getElementById("paraname");
+          // console.log(user);
+          // user.innerHTML += localStorage.getItem("name");
+          // accountExists = true;
+          // window.location.href = "index.html"
 
-            if (!accountExists) {
-                errorMsg.show();
-                errorMsg.html("Wrong username or password?");
-                errorMsg.css("color", "red");
-            }
-        });
+          // console.log(localStorage.getItem("userAccount"));
+        }
+      });
+
+      if (!accountExists) {
+        errorMsg.show();
+        errorMsg.html("Wrong username or password?");
+        errorMsg.css("color", "red");
+      }
     });
+  });
 });
-
 
 // AJAX TO ACCESS DATABASE
 function ajaxFuncGET() {
-    return $.ajax({
-        "async": true,
-        "crossDomain": true,
-        "url": dbURL,
-        "method": "GET",
-        "headers": {
-            "content-type": "application/json",
-            "x-apikey": APIKEY,
-            "cache-control": "no-cache"
-        }
-    });
+  return $.ajax({
+    async: true,
+    crossDomain: true,
+    url: dbURL,
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+      "x-apikey": APIKEY,
+      "cache-control": "no-cache",
+    },
+  });
 }
 
 // if (localStorage.getItem("isLoggedIn") == "true"){

372e3a63f98607c788b12e18141d323588b472d7
njosiahk <s10244345@connect.np.edu.sg>
2023-02-10 14:59:56 +0800

    merge

--- Javascript/game.js
+++ Javascript/game.js
@@ -70,3 +70,35 @@ function gunMover() {
     gun.style.left = event.clientX + "px";
   });
 }
+
+$(document).ready(function () {
+  const APIKEY = "63e217ae3bc6b255ed0c475f";
+  // const APIKEY= "63d62e7f3bc6b255ed0c43df"; (NJK)
+
+  var settings = {
+    async: true,
+    crossDomain: true,
+    // "url": "https://team7assg2-2b5b.restdb.io/rest/login",
+    url: "https://team7njks-7c44.restdb.io/rest/login?q={}&sort=moves&dir=1",
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+      "x-apikey": APIKEY,
+      "cache-control": "no-cache",
+    },
+  };
+
+  $.ajax(settings).done(function (response) {
+    limit = 5;
+    let content = "";
+    for (var i = 0; i < response.length && i < limit; i++) {
+      content = `${content}<tr id='${response[i]._id}'><td>${i + 1}</td><td>${
+        response[i].name
+      }</td>
+          <td>${response[i].moves}</td>`;
+    }
+    $("#leaderboard-list tbody").html(content);
+
+    // console.log(response);
+  });
+});
